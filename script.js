7579c77ee70afc558fbb2d25f21a47a3484e89a6
JasonGitHubLiu <jason.liu002@umb.edu>
2023-02-05 23:07:40 -0500

    Added show instructions button

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 const cells = document.querySelectorAll(".cell");
 const statusText = document.querySelector("#statusText");
 const restartButton = document.querySelector("#restartButton");
+
+
+// need to have a start game function
+
+// start with default money ($100)
+
+// when I click on on a div, I want to place money on that picture
+
+// have it ask how much money you want to put on each tile
+
+// cant bet over what you have in your balance
+
+// have a reset button
+
+// After each round, you can play again, which updates your balance
+
+function hideInstructions() {
+    var x = document.getElementById("gameInstructions");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
